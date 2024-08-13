ea15fb2f1f72baba4864b55ba04c1b2e0c3d759a
jxd <j7252008@163.com>
2024-08-13 16:30:50 +0800

    feat: support raster line and triangle

---
+++ render.hpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+
+namespace jrender {
+
+struct Point {
+    int x;
+    int y;
+};
+
+std::vector<Point> linePoints(Point&& p0, Point&& p1)
+{
+    std::vector<Point> pts;
+
+    bool steep = false;
+    if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y)) {
+        std::swap(p0.x, p0.y);
+        std::swap(p1.x, p1.y);
+        steep = true;
+    }
+    if (p0.x > p1.x) {
+        std::swap(p0.x, p1.x);
+        std::swap(p0.y, p1.y);
+    }
+    int dx = p1.x - p0.x;
+    int dy = p1.y - p0.y;
+    int derror2 = std::abs(dy) * 2;
+    int error2 = 0;
+    int y = p0.y;
+    for (int x = p0.x; x <= p1.x; x++) {
+        if (steep) {
+            pts.emplace_back(y, x);
+        } else {
+            pts.emplace_back(x, y);
+        }
+        error2 += derror2;
+        if (error2 > dx) {
+            y += (p1.y > p0.y ? 1 : -1);
+            error2 -= dx * 2;
+        }
+    }
+
+    return pts;
+}
+
+}
